4fe8d2da8085b6e3cb5a54280c917a7f1fa66a25
Jim <james.cullen@globalx.com.au>
2019-09-23 15:36:48 +1000

    chore: fix test formatting

--- src/PockyBot.NET.Tests/Services/TriggerResponseTesterTests.cs
+++ src/PockyBot.NET.Tests/Services/TriggerResponseTesterTests.cs
@@ -18,7 +18,7 @@ namespace PockyBot.NET.Tests.Services
         private readonly TriggerResponseTester _subject;
 
         private readonly PockyBotSettings _settings;
-        private readonly IPockyUserRepository _pockyUserRespository;
+        private readonly IPockyUserRepository _pockyUserRepository;
 
         private Message _message;
         private ITrigger _trigger;
@@ -27,15 +27,17 @@ namespace PockyBot.NET.Tests.Services
         public TriggerResponseTesterTests()
         {
             _settings = new PockyBotSettings();
-            _pockyUserRespository = Substitute.For<IPockyUserRepository>();
+            _pockyUserRepository = Substitute.For<IPockyUserRepository>();
             var options = Substitute.For<IOptions<PockyBotSettings>>();
             options.Value.Returns(_settings);
-            _subject = new TriggerResponseTester(options, _pockyUserRespository);
+            _subject = new TriggerResponseTester(options, _pockyUserRepository);
         }
 
         [Theory]
-        [MemberData(nameof(TriggerResponseTesterTestData.RoomTestData), MemberType = typeof(TriggerResponseTesterTestData))]
-        internal void TestShouldTriggerInRoom(PockyBotSettings settings, PockyUser user, Message message, ITrigger trigger, bool shouldTrigger)
+        [MemberData(nameof(TriggerResponseTesterTestData.RoomTestData), MemberType =
+            typeof(TriggerResponseTesterTestData))]
+        internal void TestShouldTriggerInRoom(PockyBotSettings settings, PockyUser user, Message message,
+            ITrigger trigger, bool shouldTrigger)
         {
             this.Given(x => GivenPockyBotSettings(settings))
                 .And(x => GivenAPockyUser(user))
@@ -47,8 +49,10 @@ namespace PockyBot.NET.Tests.Services
         }
 
         [Theory]
-        [MemberData(nameof(TriggerResponseTesterTestData.DirectMessageTestData), MemberType = typeof(TriggerResponseTesterTestData))]
-        internal void TestShouldTriggerInDirectMessage(PockyUser user, Message message, ITrigger trigger, bool shouldTrigger)
+        [MemberData(nameof(TriggerResponseTesterTestData.DirectMessageTestData), MemberType =
+            typeof(TriggerResponseTesterTestData))]
+        internal void TestShouldTriggerInDirectMessage(PockyUser user, Message message, ITrigger trigger,
+            bool shouldTrigger)
         {
             this.Given(x => GivenAPockyUser(user))
                 .And(x => GivenAMessage(message))
@@ -67,7 +71,7 @@ namespace PockyBot.NET.Tests.Services
 
         private void GivenAPockyUser(PockyUser user)
         {
-            _pockyUserRespository.GetUser(user.UserId).Returns(user);
+            _pockyUserRepository.GetUser(user.UserId).Returns(user);
         }
 
         private void GivenAMessage(Message message)
