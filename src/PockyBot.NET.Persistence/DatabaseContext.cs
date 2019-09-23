be98e455fb52ca7ef60b1b8d9fbf2f0ca57ad242
Laura Berger-Uichanco <laura-h-95@live.com.au>
2019-09-23 20:14:26 +1000

    fix: update db representation for foreign keys

--- src/PockyBot.NET.Persistence/DatabaseContext.cs
+++ src/PockyBot.NET.Persistence/DatabaseContext.cs
@@ -7,6 +7,14 @@ namespace PockyBot.NET.Persistence
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Role>().HasKey(table => new
+            {
+                table.UserId, table.UserRole
+            });
+        }
+
         public DbSet<PockyUser> PockyUsers { get; set; }
         public DbSet<Role> Roles { get; set; }
     }
--- src/PockyBot.NET.Persistence/Models/Role.cs
+++ src/PockyBot.NET.Persistence/Models/Role.cs
@@ -5,10 +5,11 @@ namespace PockyBot.NET.Persistence.Models
     [Table("roles")]
     public class Role
     {
-        [ForeignKey("userid")]
         [Column("userid")]
         public string UserId { get; set; }
         [Column("role")]
         public string UserRole { get; set; }
+
+        public PockyUser User { get; set; }
     }
 }
