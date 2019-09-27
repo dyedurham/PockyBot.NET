14638479cdd1aa795fb190bc23b0a1197be31872
Laura Berger-Uichanco <laura-h-95@live.com.au>
2019-09-27 20:14:13 +1000

    test: add tests for peg request validation

---
+++ src/PockyBot.NET.Tests/Services/Pegs/PegRequestValidatorTests.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using GlobalX.ChatBots.Core.Messages;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using PockyBot.NET.Configuration;
+using PockyBot.NET.Persistence.Repositories;
+using PockyBot.NET.Services.Pegs;
+using PockyBot.NET.Tests.TestData.Pegs;
+using Shouldly;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace PockyBot.NET.Tests.Services.Pegs
+{
+    public class PegRequestValidatorTests
+    {
+        private readonly PegRequestValidator _subject;
+
+        private readonly PockyBotSettings _settings;
+        private readonly IConfigRepository _configRepository;
+
+        private Message _message;
+        private bool _result;
+        private string _errorMessage;
+
+        public PegRequestValidatorTests()
+        {
+            _settings = new PockyBotSettings();
+            _configRepository = Substitute.For<IConfigRepository>();
+            var options = Substitute.For<IOptions<PockyBotSettings>>();
+            options.Value.Returns(_settings);
+            _subject = new PegRequestValidator(options, _configRepository);
+        }
+
+        [Theory]
+        [MemberData(nameof(PegRequestValidatorTestData.ValidTestData), MemberType = typeof(PegRequestValidatorTestData))]
+        public void TestValidateValidPegRequest(PockyBotSettings settings, int commentsRequired, List<string> keywords, List<string> penaltyKeywords, int requireValues, Message message)
+        {
+            this.Given(x => GivenPockyBotSettings(settings))
+                .And(x => GivenAConfig(commentsRequired, keywords, penaltyKeywords, requireValues))
+                .And(x => GivenAMessage(message))
+                .When(x => WhenValidatingAPegRequest())
+                .Then(x => ThenTheMessageShouldBeValid())
+                .And(x => ThenTheErrorMessageShouldBeNull())
+                .BDDfy();
+        }
+
+        [Theory]
+        [MemberData(nameof(PegRequestValidatorTestData.InvalidTestData), MemberType = typeof(PegRequestValidatorTestData))]
+        public void TestValidateInvalidPegRequest(PockyBotSettings settings, int commentsRequired, List<string> keywords, List<string> penaltyKeywords, int requireValues, Message message, string errorMessage)
+        {
+            this.Given(x => GivenPockyBotSettings(settings))
+                .And(x => GivenAConfig(commentsRequired, keywords, penaltyKeywords, requireValues))
+                .And(x => GivenAMessage(message))
+                .When(x => WhenValidatingAPegRequest())
+                .Then(x =>ThenTheMessageShouldBeInvalid())
+                .And(x => ThenTheErrorMessageShouldBe(errorMessage))
+                .BDDfy();
+        }
+
+        private void GivenPockyBotSettings(PockyBotSettings settings)
+        {
+            _settings.BotId = settings.BotId;
+            _settings.BotName = settings.BotName;
+            _settings.DatabaseConnectionString = settings.DatabaseConnectionString;
+        }
+
+        private void GivenAConfig(int commentsRequired, List<string> keywords, List<string> penaltyKeywords, int requireValues)
+        {
+            _configRepository.GetGeneralConfig("commentsRequired").Returns(commentsRequired);
+            _configRepository.GetStringConfig("keyword").Returns(keywords);
+            _configRepository.GetStringConfig("penaltyKeyword").Returns(penaltyKeywords);
+            _configRepository.GetGeneralConfig("requireValues").Returns(requireValues);
+        }
+
+        private void GivenAMessage(Message message)
+        {
+            _message = message;
+        }
+
+        private void WhenValidatingAPegRequest()
+        {
+            _result = _subject.ValidatePegRequest(_message, out _errorMessage);
+        }
+
+        private void ThenTheMessageShouldBeValid()
+        {
+            _result.ShouldBeTrue();
+        }
+
+        private void ThenTheMessageShouldBeInvalid()
+        {
+            _result.ShouldBeFalse();
+        }
+
+        private void ThenTheErrorMessageShouldBeNull()
+        {
+            _errorMessage.ShouldBeNull();
+        }
+
+        private void ThenTheErrorMessageShouldBe(string errorMessage)
+        {
+            _errorMessage.ShouldBe(errorMessage);
+        }
+    }
+}
