adf9d8f99fdd4fa6c27a76cda50b0fb7e374c358
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2019-11-01 15:29:13 +1000

    test: add tests for status trigger

---
+++ src/PockyBot.NET.Tests/Services/Triggers/StatusTests.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using GlobalX.ChatBots.Core.Messages;
+using NSubstitute;
+using PockyBot.NET.Persistence.Models;
+using PockyBot.NET.Persistence.Repositories;
+using PockyBot.NET.Services.Pegs;
+using PockyBot.NET.Services.Triggers;
+using PockyBot.NET.Tests.TestData.Triggers;
+using Shouldly;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace PockyBot.NET.Tests.Services.Triggers
+{
+    public class StatusTests
+    {
+        private readonly Status _subject;
+
+        private readonly IPockyUserRepository _pockyUserRepository;
+        private readonly IConfigRepository _configRepository;
+        private readonly IPegCommentValidator _pegCommentValidator;
+
+        private Message _message;
+        private Message _result;
+
+        public StatusTests()
+        {
+            _pockyUserRepository = Substitute.For<IPockyUserRepository>();
+            _configRepository = Substitute.For<IConfigRepository>();
+            _pegCommentValidator = Substitute.For<IPegCommentValidator>();
+            _subject = new Status(_pockyUserRepository, _configRepository, _pegCommentValidator);
+        }
+
+        [Theory]
+        [MemberData(nameof(StatusTestData.RespondTestData), MemberType = typeof(StatusTestData))]
+        public void TestRespond(Message message, PockyUser sender, int limit, List<Tuple<string, bool>> pegValidity, Message expectedResponse)
+        {
+            this.Given(x => GivenAMessage(message))
+                .And(x => GivenAPockyUser(sender))
+                .And(x => GivenAStringConfig("keyword", new List<string> { "keyword1", "keyword2" }))
+                .And(x => GivenAStringConfig("penaltyKeyword", new List<string> { "penaltyKeyword" }))
+                .And(x => GivenAGeneralConfig("requireValues", 1))
+                .And(x => GivenAGeneralConfig("limit", limit))
+                .And(x => GivenPegValidity(pegValidity))
+                .When(x => WhenRespondingToAMessage())
+                .Then(x => ThenItShouldReturnAResponse(expectedResponse))
+                .BDDfy();
+        }
+
+        private void GivenAMessage(Message message)
+        {
+            _message = message;
+        }
+
+        private void GivenAPockyUser(PockyUser user)
+        {
+            if (user != null)
+            {
+                _pockyUserRepository.GetUser(user.UserId).Returns(user);
+            }
+            else
+            {
+                _pockyUserRepository.GetUser(Arg.Any<string>()).ReturnsForAnyArgs((PockyUser)null);
+            }
+        }
+
+        private void GivenAStringConfig(string configName, List<string> config)
+        {
+            _configRepository.GetStringConfig(configName).Returns(config);
+        }
+
+        private void GivenAGeneralConfig(string configName, int config)
+        {
+            _configRepository.GetGeneralConfig(configName).Returns(config);
+        }
+
+        private void GivenPegValidity(List<Tuple<string, bool>> pegValidity)
+        {
+            foreach (var peg in pegValidity)
+            {
+                _pegCommentValidator.IsPegValid(peg.Item1, Arg.Any<int>(), Arg.Any<string[]>(), Arg.Any<string[]>())
+                    .Returns(peg.Item2);
+            }
+        }
+
+        private async void WhenRespondingToAMessage()
+        {
+            _result = await _subject.Respond(_message);
+        }
+
+        private void ThenItShouldReturnAResponse(Message expectedResponse)
+        {
+            _result.ShouldNotBeNull();
+            _result.Text.ShouldBe(expectedResponse.Text);
+        }
+    }
+}
---
+++ src/PockyBot.NET.Tests/TestData/Triggers/StatusTestData.cs
@@ -0,0 +1,418 @@
+using System;
+using System.Collections.Generic;
+using GlobalX.ChatBots.Core.Messages;
+using PockyBot.NET.Constants;
+using PockyBot.NET.Persistence.Models;
+
+namespace PockyBot.NET.Tests.TestData.Triggers
+{
+    public static class StatusTestData
+    {
+        public static IEnumerable<object[]> RespondTestData()
+        {
+            // No pegs given
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>(),
+                    Roles = new List<Role>()
+                },
+                5, // limit
+                new List<Tuple<string, bool>>(),
+                new Message
+                {
+                    Text = "You have 5 pegs left to give.\n\nYou have not given any pegs so far."
+                }
+            };
+
+            // user does not exist
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                null,
+                5, // limit
+                new List<Tuple<string, bool>>(),
+                new Message
+                {
+                    Text = "You have 5 pegs left to give.\n\nYou have not given any pegs so far."
+                }
+            };
+
+            // Some pegs given, no penalties
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>
+                    {
+                        new Peg{ Comment = "This is a comment", Receiver = new PockyUser { Username = "Test User 2" } },
+                        new Peg{ Comment = "This is another comment", Receiver = new PockyUser { Username = "Test User 3" } }
+                    },
+                    Roles = new List<Role>()
+                },
+                5, // limit
+                new List<Tuple<string, bool>>
+                {
+                    new Tuple<string, bool>("This is a comment", true),
+                    new Tuple<string, bool>("This is another comment", true)
+                },
+                new Message
+                {
+                    Text = "You have 3 pegs left to give.\n\nHere are the pegs you've given so far:\n* **Test User 2 — \"_This is a comment_\"\n* **Test User 3 — \"_This is another comment_\"\n"
+                }
+            };
+
+            // Some penalties, no pegs
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>
+                    {
+                        new Peg{ Comment = "Shame shame", Receiver = new PockyUser { Username = "Test User 4" } },
+                        new Peg{ Comment = "More shame", Receiver = new PockyUser { Username = "Test User 5" } }
+                    },
+                    Roles = new List<Role>()
+                },
+                5, // limit
+                new List<Tuple<string, bool>>
+                {
+                    new Tuple<string, bool>("Shame shame", false),
+                    new Tuple<string, bool>("More shame", false)
+                },
+                new Message
+                {
+                    Text = "You have 5 pegs left to give.\n\nHere are the penalties you have received:\n* **Test User 4 — \"_Shame shame_\"\n* **Test User 5 — \"_More shame_\"\n"
+                }
+            };
+
+            // Some pegs and penalties given
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>
+                    {
+                        new Peg{ Comment = "This is a comment", Receiver = new PockyUser { Username = "Test User 2" } },
+                        new Peg{ Comment = "This is another comment", Receiver = new PockyUser { Username = "Test User 3" } },
+                        new Peg{ Comment = "Shame shame", Receiver = new PockyUser { Username = "Test User 4" } },
+                        new Peg{ Comment = "More shame", Receiver = new PockyUser { Username = "Test User 5" } }
+                    },
+                    Roles = new List<Role>()
+                },
+                5, // limit
+                new List<Tuple<string, bool>>
+                {
+                    new Tuple<string, bool>("This is a comment", true),
+                    new Tuple<string, bool>("This is another comment", true),
+                    new Tuple<string, bool>("Shame shame", false),
+                    new Tuple<string, bool>("More shame", false)
+                },
+                new Message
+                {
+                    Text = "You have 3 pegs left to give.\n\nHere are the pegs you've given so far:\n* **Test User 2 — \"_This is a comment_\"\n* **Test User 3 — \"_This is another comment_\"\n\n\nHere are the penalties you have received:\n* **Test User 4 — \"_Shame shame_\"\n* **Test User 5 — \"_More shame_\"\n"
+                }
+            };
+
+            // Unmetered user
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>(),
+                    Roles = new List<Role>
+                    {
+                        new Role { UserRole = Roles.Unmetered }
+                    }
+                },
+                5, // limit
+                new List<Tuple<string, bool>>(),
+                new Message
+                {
+                    Text = "You have unlimited pegs left to give.\n\nYou have not given any pegs so far."
+                }
+            };
+
+            // Unmetered user, some pegs given
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>
+                    {
+                        new Peg{ Comment = "This is a comment", Receiver = new PockyUser { Username = "Test User 2" } },
+                        new Peg{ Comment = "This is another comment", Receiver = new PockyUser { Username = "Test User 3" } }
+                    },
+                    Roles = new List<Role>
+                    {
+                        new Role { UserRole = Roles.Unmetered }
+                    }
+                },
+                5, // limit
+                new List<Tuple<string, bool>>
+                {
+                    new Tuple<string, bool>("This is a comment", true),
+                    new Tuple<string, bool>("This is another comment", true)
+                },
+                new Message
+                {
+                    Text = "You have unlimited pegs left to give.\n\nHere are the pegs you've given so far:\n* **Test User 2 — \"_This is a comment_\"\n* **Test User 3 — \"_This is another comment_\"\n"
+                }
+            };
+
+            // Unmetered user, Some penalties, no pegs
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>
+                    {
+                        new Peg{ Comment = "Shame shame", Receiver = new PockyUser { Username = "Test User 4" } },
+                        new Peg{ Comment = "More shame", Receiver = new PockyUser { Username = "Test User 5" } }
+                    },
+                    Roles = new List<Role>
+                    {
+                        new Role{ UserRole = Roles.Unmetered }
+                    }
+                },
+                5, // limit
+                new List<Tuple<string, bool>>
+                {
+                    new Tuple<string, bool>("Shame shame", false),
+                    new Tuple<string, bool>("More shame", false)
+                },
+                new Message
+                {
+                    Text = "You have unlimited pegs left to give.\n\nHere are the penalties you have received:\n* **Test User 4 — \"_Shame shame_\"\n* **Test User 5 — \"_More shame_\"\n"
+                }
+            };
+
+            // Unmetered user, some pegs and penalties given
+            yield return new object[]
+            {
+                new Message
+                {
+                    Text = "TestBot status",
+                    MessageParts = new[]
+                    {
+                        new MessagePart
+                        {
+                            Text = "TestBot",
+                            MessageType = MessageType.PersonMention,
+                            UserId = "testBotId"
+                        },
+                        new MessagePart
+                        {
+                            Text = " status",
+                            MessageType = MessageType.Text
+                        }
+                    },
+                    SenderId = "testUserId",
+                    SenderName = "Test User"
+                },
+                new PockyUser // sender
+                {
+                    UserId = "testUserId",
+                    Username = "Test User",
+                    PegsGiven = new List<Peg>
+                    {
+                        new Peg{ Comment = "This is a comment", Receiver = new PockyUser { Username = "Test User 2" } },
+                        new Peg{ Comment = "This is another comment", Receiver = new PockyUser { Username = "Test User 3" } },
+                        new Peg{ Comment = "Shame shame", Receiver = new PockyUser { Username = "Test User 4" } },
+                        new Peg{ Comment = "More shame", Receiver = new PockyUser { Username = "Test User 5" } }
+                    },
+                    Roles = new List<Role>
+                    {
+                        new Role{ UserRole = Roles.Unmetered }
+                    }
+                },
+                5, // limit
+                new List<Tuple<string, bool>>
+                {
+                    new Tuple<string, bool>("This is a comment", true),
+                    new Tuple<string, bool>("This is another comment", true),
+                    new Tuple<string, bool>("Shame shame", false),
+                    new Tuple<string, bool>("More shame", false)
+                },
+                new Message
+                {
+                    Text = "You have unlimited pegs left to give.\n\nHere are the pegs you've given so far:\n* **Test User 2 — \"_This is a comment_\"\n* **Test User 3 — \"_This is another comment_\"\n\n\nHere are the penalties you have received:\n* **Test User 4 — \"_Shame shame_\"\n* **Test User 5 — \"_More shame_\"\n"
+                }
+            };
+        }
+    }
+}
