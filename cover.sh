5029910c2a6fe87e3688581375a0a69d0aea5e5c
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2019-11-04 10:21:55 +1000

    ci: add codacy code coverage reporting

--- cover.sh
+++ cover.sh
@@ -2,3 +2,8 @@
 
 dotnet test src/PockyBot.NET.Tests/PockyBot.NET.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="opencover"
 bash <(curl -s https://codecov.io/bash) -f src/PockyBot.NET.Tests/coverage.opencover.xml
+
+# Codacy
+curl -Ls "https://github.com/codacy/csharp-codacy-coverage/releases/download/$CODACY_COVERAGE_VERSION/Codacy.CSharpCoverage_linux-x64" --output "Codacy.CSharpCoverage_linux-x64"
+chmod +x ./Codacy.CSharpCoverage_linux-x64
+./Codacy.CSharpCoverage_linux-x64 -c "$TRAVIS_COMMIT" -t "$CODACY_PROJECT_TOKEN" -r src/PockyBot.NET.Tests/coverage.opencover.xml -e opencover
