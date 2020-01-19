cfc4a666ad2b57b5ca8b1ab30a5cf5993a588467
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2020-01-20 09:34:59 +1000

    chore: uncomment release code

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Dry run semantic-release to get next version number
-VERSION="$(npx -p semantic-release@15.13.19 -p @semantic-release/changelog@3.0.4 -p @semantic-release/git@7.0.16 \
--p @semantic-release/exec@3.3.5 -p @semantic-release/github@5.4.2 semantic-release --dry-run | \
+VERSION="$(npx -p semantic-release@16.0.2 -p @semantic-release/changelog@3.0.6 -p @semantic-release/git@8.0.0 \
+-p @semantic-release/exec@4.0.0 -p @semantic-release/github@6.0.1 semantic-release --dry-run | \
 grep "next release version is" | sed -n "s/^.*next release version is\s*\(\S*\).*$/\1/p")"
 
 if [ -z "$VERSION" ]
@@ -16,6 +16,5 @@ echo "The next release number is $VERSION"
 mkdir -p ./artifacts
 dotnet pack "src/PockyBot.NET/PockyBot.NET.csproj" -p:PackageVersion="$VERSION" -o "../../artifacts"
 
-# UNCOMMENT WHEN READY FOR RELEASE
-# npx -p semantic-release@15.13.19 -p @semantic-release/changelog@3.0.4 -p @semantic-release/git@7.0.16 \
-# -p @semantic-release/exec@3.3.5 -p @semantic-release/github@5.4.2 semantic-release
+npx -p semantic-release@16.0.2 -p @semantic-release/changelog@3.0.6 -p @semantic-release/git@8.0.0 \
+-p @semantic-release/exec@4.0.0 -p @semantic-release/github@6.0.1 semantic-release
