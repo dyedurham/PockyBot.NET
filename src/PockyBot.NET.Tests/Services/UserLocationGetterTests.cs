55ee54e393479f5bb6374e37e6ce3d24749007d8
James Cullen <james.cullen@globalx.com.au>
2020-03-04 16:43:54 +1000

    test(user-location-getter): test the basic cases

--- src/PockyBot.NET.Tests/Services/UserLocationGetterTests.cs
+++ src/PockyBot.NET.Tests/Services/UserLocationGetterTests.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using Shouldly;
 using TestStack.BDDfy;
 using Xunit;
 using NSubstitute;
+using PockyBot.NET.Persistence.Models;
 using PockyBot.NET.Persistence.Repositories;
 using PockyBot.NET.Services;
 
@@ -25,12 +27,42 @@ namespace PockyBot.NET.Tests.Services
         }
 
         [Fact]
-        void NoCommandsShouldReturnAnError()
+        internal void NoCommandsShouldReturnAnError()
         {
             this.Given(x => GivenNoCommands())
                 .When(x => WhenGetUserLocationIsCalled())
                 .Then(x => ThenItShouldReturn(
                     "Please specify a user or group of users. Possible arguments are me, unset, all, or a list of mentioned users."))
+                .BDDfy();
+        }
+
+        [Fact]
+        internal void MeCommandShouldReturnUsersLocation()
+        {
+            this.Given(x => GivenCommand("me"))
+                .And(x => GivenUserHasLocation("myLocation"))
+                .When(x => WhenGetUserLocationIsCalled())
+                .Then(x => ThenItShouldReturn("Your location is myLocation"))
+                .BDDfy();
+        }
+
+        [Fact]
+        internal void AllCommandShouldReturnAllUserLocations()
+        {
+            this.Given(x => GivenCommand("all"))
+                .And(x => GivenSomeUsersHaveLocations())
+                .When(x => WhenGetUserLocationIsCalled())
+                .Then(x => ThenItShouldReturnAllLocations())
+                .BDDfy();
+        }
+
+        [Fact]
+        internal void UnsetCommandShouldReturnUnsetUsers()
+        {
+            this.Given(x => GivenCommand("unset"))
+                .And(x => GivenSomeUsersHaveLocations())
+                .When(x => WhenGetUserLocationIsCalled())
+                .Then(x => ThenItShouldReturnUnsetUsers())
                 .BDDfy();
         }
 
@@ -41,6 +73,29 @@ namespace PockyBot.NET.Tests.Services
             _meId = "";
         }
 
+        private void GivenCommand(string command)
+        {
+            _commands = new[] { command };
+            _mentionedUsers = new string[] { };
+            _meId = "me";
+        }
+
+        private void GivenUserHasLocation(string location)
+        {
+            _pockyUserRepository.GetUser(Arg.Any<string>()).Returns(GetUser("me", location));
+        }
+
+        private void GivenSomeUsersHaveLocations()
+        {
+            _pockyUserRepository.GetAllUsersLocations().Returns(new List<PockyUser>
+            {
+                GetUser("User1", "Location1"),
+                GetUser("User2", null),
+                GetUser("User3", "Location3"),
+                GetUser("User4", "")
+            });
+        }
+
         private void WhenGetUserLocationIsCalled()
         {
             _result = _subject.GetUserLocation(_commands, _mentionedUsers, _meId);
@@ -49,6 +104,37 @@ namespace PockyBot.NET.Tests.Services
         private void ThenItShouldReturn(string expected)
         {
             _result.ShouldBe(expected);
+        }
+
+        private void ThenItShouldReturnAllLocations()
+        {
+            _result.ShouldContain("Here are all users' locations:");
+            _result.ShouldContain("* **User1**: Location1");
+            _result.ShouldContain("* **User3**: Location3");
+            _result.ShouldNotContain("User2");
+            _result.ShouldNotContain("User4");
+        }
+
+        private void ThenItShouldReturnUnsetUsers()
+        {
+            _result.ShouldContain("Here are the users without a location set:");
+            _result.ShouldContain("* User2");
+            _result.ShouldContain("* User4");
+            _result.ShouldNotContain("User1");
+            _result.ShouldNotContain("User3");
+        }
+
+        private PockyUser GetUser(string username, string location)
+        {
+            return new PockyUser
+            {
+                Username = username,
+                UserId = username,
+                Location = new UserLocation
+                {
+                    Location = location
+                }
+            };
         }
     }
 }
