ab778943faba088aac8dafaa83fb44da51446fab
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2020-03-11 15:46:41 +1000

    chore: update deploy step

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ fi
 echo "The next release number is $VERSION"
 
 mkdir -p ./artifacts
-dotnet pack "src/PockyBot.NET/PockyBot.NET.csproj" -p:PackageVersion="$VERSION" -o "../../artifacts"
+dotnet pack "src/PockyBot.NET/PockyBot.NET.csproj" -p:PackageVersion="$VERSION" -o "./artifacts"
 
 npx -p semantic-release@16.0.2 -p @semantic-release/changelog@3.0.6 -p @semantic-release/git@8.0.0 \
 -p @semantic-release/exec@4.0.0 -p @semantic-release/github@6.0.1 semantic-release
