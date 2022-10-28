cfe2202e7bb550b8c7f8aa587215f1acfa791344
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2022-10-28 12:01:59 +1000

    chore: add commit sha to codecov

--- cover.sh
+++ cover.sh
@@ -3,7 +3,7 @@
 dotnet test src/PockyBot.NET.Tests/PockyBot.NET.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="opencover"
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 chmod +x codecov
-./codecov -f src/PockyBot.NET.Tests/coverage.opencover.xml -t $CODECOV_TOKEN
+./codecov -f src/PockyBot.NET.Tests/coverage.opencover.xml -t $CODECOV_TOKEN --sha $COMMIT_SHA
 
 # Codacy
 bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r src/PockyBot.NET.Tests/coverage.opencover.xml
