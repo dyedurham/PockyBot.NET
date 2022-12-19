0a92ff42c5cb00cd8abd99f12f8f8feedc426a5b
Evelyne Grier <evelyne.grier@globalx.com.au>
2022-12-19 13:57:14 +1000

    fix: update cover script to run in docker container to fix coverage report

--- cover.sh
+++ cover.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dotnet test src/PockyBot.NET.Tests/PockyBot.NET.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="opencover"
+docker run -v $(pwd)/src:/src mcr.microsoft.com/dotnet/sdk:6.0-alpine dotnet test src/PockyBot.NET.Tests/PockyBot.NET.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="opencover"
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 chmod +x codecov
 ./codecov -f src/PockyBot.NET.Tests/coverage.opencover.xml -t $CODECOV_TOKEN --sha $COMMIT_SHA
